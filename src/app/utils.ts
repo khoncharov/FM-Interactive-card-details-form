78c1701d0d616d8ed897ba28cb9b89f7ddd903cf
Konstantin Honcharov <k.honcharov@gmail.com>
2023-01-20 16:33:51 +0300

    feat: add util functions

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,1 +1,25 @@
 export const isNotEmptyStr = (value: string): boolean => Boolean(value.length);
+
+export const formatCardNumber = (value: string): string => {
+  const lettersArray = value.split('');
+
+  const result: string[] = [];
+  const a: string[] = [];
+
+  const last = lettersArray.reduce((acc, item) => {
+    if (acc.length < 4) {
+      acc.push(item);
+    } else {
+      result.push(acc.join(''));
+      acc.length = 0;
+      acc.push(item);
+    }
+    return acc;
+  }, a);
+
+  result.push(last.join(''));
+
+  return result.join(' ');
+};
+
+export const deleteSpaces = (value: string): string => value.trim().split(' ').join('');
