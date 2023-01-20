4db413d5da6444255e8c98e3fec723b842f51f5c
Konstantin Honcharov <k.honcharov@gmail.com>
2023-01-21 01:08:31 +0300

    feat: add validation messages

--- src/app/const.ts
+++ src/app/const.ts
@@ -3,3 +3,9 @@ export const DEFAULT_CARDHOLDER = 'Jane Appleseed';
 export const DEFAULT_CARD_EXP_MONTH = '00';
 export const DEFAULT_CARD_EXP_YEAR = '00';
 export const DEFAULT_CARD_CVC = '000';
+
+export const MSG_REQUIRED_FIELD = `Can't be blank`;
+export const MSG_WRONG_NAME = 'Latin letters, space and hyphen only';
+export const MSG_WRONG_NUMBER = 'Numbers only';
+export const MSG_WRONG_LENGTH = (length: number) => `Must be ${length} digit number`;
+export const MSG_WRONG_MONTH = 'Wrong month number';
